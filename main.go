d70ca77c72466ab865f130e8fbbd6f2966b3256b
manthanchauhan <manthan.chauhan@myparkplus.com>
2021-05-30 14:12:34 +0530

    set db for all requests.

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/manthanchauhan/gin_getting_started/db"
+	"github.com/manthanchauhan/gin_getting_started/middlewares"
 )
 
 var router *gin.Engine
 
 func main() {
 	database := db.Connect()
+
 	router = gin.Default()
+	router.Use(middlewares.SetDBToContext(database))
 
 	initializeRouter()
 
